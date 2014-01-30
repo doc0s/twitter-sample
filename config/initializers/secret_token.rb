0aaf69942fe0732f06e44c987db541f3b9d76849
Shinya Doko <shinya.doko@sonymobile.com>
2014-01-30 19:27:33 +0900

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tweet::Application.config.secret_key_base = 'b47d855cb3673def74cbd665711a294f2e6caea84ef27164f89e3288e9b91a2a8ba3c39e4e0fa55cb09028e0985b21621ca5d27bd751804f99840be322f81d32'
