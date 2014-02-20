86d82f59630d8d8d88bf2a4204a3175972be0791
doco <sn.doco@gmail.com>
2014-02-20 15:52:44 +0900

    Fixed secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
 Tweet::Application.config.secret_key_base = 'b47d855cb3673def74cbd665711a294f2e6caea84ef27164f89e3288e9b91a2a8ba3c39e4e0fa55cb09028e0985b21621ca5d27bd751804f99840be322f81d32'
