6b1dc1db592b2cb64385850afe97aaa7b3c769fe
doc0s <github.doc0s@gmail.com>
2014-07-31 16:04:58 +0900

    Lower bcrypt cost

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Tweet::Application.initialize!
+
+# Lower the cost of bcrypt
+ActiveModel::SecurePassword.min_cost = true
