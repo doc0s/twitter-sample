d7b1ce538c98ffb35eed0a7fe1b26b6644165a20
doc0s <github.doc0s@gmail.com>
2015-03-13 14:34:36 +0900

    Add user index page 9.3.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-    before_action :signed_in_user, only: [:edit, :update]
+    before_action :signed_in_user, only: [:index, :edit, :update]
     before_action :correct_user, only: [:edit, :update]
 
     def show
@@ -32,6 +32,14 @@ class UsersController < ApplicationController
         else
             render 'edit'
         end
+    end
+
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
     end
 
     private
